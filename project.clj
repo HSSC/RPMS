9d9c1decf32c32cfd1bbb57779645db1f433c3c7
dfr2 <rugg@musc.edu>
2012-02-29 22:25:37 -0500

    Commit the parent project builder.

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject org.healthsciencessc.rpms2/parent "1.0.0-SNAPSHOT"
+  :description "Consent Parent Builder"
+  :omit-default-repositories true
+  :dev-dependencies [[lein-sub "0.1.2"]]
+  :sub ["consent-services"]) 
