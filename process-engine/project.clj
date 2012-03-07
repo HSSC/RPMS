ed620183fe7b60178d7368a421b5f8a911f02bf7
Colin Alstad <colin.alstad@gmail.com>
2012-03-07 13:12:03 -0500

    AOT process engine to play nice with when a dependency in a war.

--- process-engine/project.clj
+++ process-engine/project.clj
@@ -1,3 +1,4 @@
 (defproject org.healthsciencessc.rpms2/process-engine "1.0.0-SNAPSHOT"
   :description "Process Engine For RPMS Applications"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :aot [org.healthsciencessc.rpms2.process-engine.core])
