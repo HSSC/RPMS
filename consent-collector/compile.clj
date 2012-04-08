331fe413b5be086997666be6600ca39760bfa290
dfr2 <rugg@musc.edu>
2012-04-08 18:55:18 -0400

    Adding the ClojureScript compilation to the build cycle.

---
+++ consent-collector/compile.clj
@@ -0,0 +1,19 @@
+(require :reload '[cljs.compiler :as comp] '[clojure.java.io :as io] '[cljs.core])
+(import '(com.google.javascript.jscomp JSSourceFile CompilerOptions CompilationLevel 
+                                       ClosureCodingConvention))
+
+(let [in "src/main/script/org/healthsciencessc/rpms2/core.cljs"
+    	out "src/main/script/org/healthsciencessc/rpms2/core.js"
+      target "src/main/webapp/app.js"
+      options []]
+  (comp/compile-file in out)
+  (let [source (slurp out)
+        options (CompilerOptions.)
+        level CompilationLevel/WHITESPACE_ONLY
+        extern (JSSourceFile/fromCode "externs.js" "function alert(x) {}")
+        input (JSSourceFile/fromCode "input.js" source)
+        compiler (com.google.javascript.jscomp.Compiler.)]
+    (.setCodingConvention options (ClosureCodingConvention.))
+    (.setPrettyPrint options true)
+    (.compile compiler extern input options)
+    (spit target (.toSource compiler))))
