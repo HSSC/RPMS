9db2907f626859f70e3abf712c7a33643bf70763
dfr2 <rugg@musc.edu>
2012-04-19 10:45:45 -0400

    Modifying props so they can be accessed by keyword or map function.

--- consent-domain/src/main/clojure/org/healthsciencessc/rpms2/consent_domain/props.clj
+++ consent-domain/src/main/clojure/org/healthsciencessc/rpms2/consent_domain/props.clj
@@ -8,7 +8,8 @@
   "Loads the key/values found in a properties file found on the classpath into a map"
   [url]
   (try
-    (into {} (doto (java.util.Properties.)(.load (resource url))))
+    (let [props (into {} (doto (java.util.Properties.)(.load (resource url))))]
+      (concat props (for [[k v] props] [(keyword k) v])))
     (catch Exception e {})))
 
 (defn- self
