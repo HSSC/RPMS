70bc80e08b4ba5a58cb3640eb82ed1eeaf3c181d
dfr2 <rugg@musc.edu>
2012-04-30 14:51:17 -0400

    Fixing POMs and tests so all applications build successfully.

--- consent-admin/src/test/clojure/org/healthsciencessc/rpms2/consent_admin/test/core.clj
+++ consent-admin/src/test/clojure/org/healthsciencessc/rpms2/consent_admin/test/core.clj
@@ -2,5 +2,5 @@
   (:use [org.healthsciencessc.rpms2.consent-admin.core])
   (:use [clojure.test]))
 
-;;(deftest replace-me ;; FIXME: write
-  ;;(is false "No tests have been written."))
+(deftest replace-me ;; FIXME: write
+  (is true "No tests have been written."))
