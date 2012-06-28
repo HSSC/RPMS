da6be57a25164acb187ea259c96feb1b8c351b19
dfr2 <rugg@musc.edu>
2012-06-28 17:16:26 -0400

    Converted response to play nice with new response format. Additional style changes.

--- consent-admin/src/main/clojure/org/healthsciencessc/rpms2/consent_admin/ajax.clj
+++ consent-admin/src/main/clojure/org/healthsciencessc/rpms2/consent_admin/ajax.clj
@@ -1,36 +1,27 @@
 (ns org.healthsciencessc.rpms2.consent-admin.ajax
-  (:require [ring.util.response :as response]
-            [clojure.data.json :as json]))
-
-(def headers {"Content-Type" "application/json"})
-
-(defn- response
-  [status body]
-  {:status status
-   :headers headers
-   :body body})
+  (:use [ring.util.response :as response]))
 
 (defn success
   "Function called to return a successful response"
   [body]
-  (response 200 body))
+  body)
 
 (defn error
   "Function called to return error."
   [{msg :message :as m}]
-  (response 500 {:message msg}))
+  (status (response/response {:message msg}) 500))
 
 (defn save-failed
   "Function called to return error."
   [{msg :message :as m}]
-  (response 500 {:message msg}))
+  (status (response/response {:message msg}) 500))
 
 (defn lookup-failed
   "Function called to return error."
   [{msg :message :as m}]
-  (response 404 {:message msg}))
+  (response/not-found {:message msg}))
 
 (defn forbidden
   "Function called to return a forbiddent error."
   [_]
-  (response 403 nil))
+  (status (response/response {}) 403))
