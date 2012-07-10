ec1f74d22395decf7fa5976fc0dea27324e85641
Ghadi Shayban <gshayban@gmail.com>
2012-07-10 15:52:56 -0400

    xml export working

--- consent-services/src/main/clojure/org/healthsciencessc/rpms2/consent_services/default_processes/protocol_version_export.clj
+++ consent-services/src/main/clojure/org/healthsciencessc/rpms2/consent_services/default_processes/protocol_version_export.clj
@@ -7,73 +7,59 @@
             [org.healthsciencessc.rpms2.consent-domain.types :as types]
             [clojure.data.xml :as xml]
             [clojure.string :as str])
-  (:import [org.healthsciencessc.rpms2.process_engine.core DefaultProcess]))
+  (:import [org.healthsciencessc.rpms2.process_engine.core DefaultProcess]
+           [clojure.data.xml Element]))
 
-(def protocol-version-elements
-  {:meta-items {:child-types [:labels]
-                :attributes [:name]
-                :node-name :MetaItem}
-   :endorsements {:child-types [:labels]
-                  :attributes [:name]
-                  :node-name :Endorsement}
-   :policies {:child-types [:texts :titles]
-              :attributes [:name]
-              :node-name :Policy}})
+(defn emit-endorsements [xs filterfn]
+  (Element. :endorsements
+                {}
+                (for [e xs]
+                  (Element. :endorsement
+                                {}
+                                (for [l (filter filterfn (:labels e))]
+                                  (Element. :label
+                                                {}
+                                                [(:value l)]))))))
 
-(defn singularize
-  [s]
-  (cond
-   (string? s) (apply str (drop-last (seq s)))
-   (keyword? s) (keyword (singularize (name s)))))
+(defn emit-metaitems [xs filterfn]
+  (Element. :meta-items
+                {}
+                (for [m xs]
+                  (Element. :meta-item
+                                {}
+                                (for [l (filter filterfn (:labels m))]
+                                  (Element. :label
+                                                {}
+                                                [(:value l)]))))))
 
-(defn camel-case
-  [s]
-  (cond
-   (string? s) (str/join (map str/capitalize (str/split (str/replace s "-" " ") #" ")))
-   (keyword? s) (keyword (camel-case (name s)))))
+(defn emit-policies [policies filterfn]
+  (Element. :policies
+                {}
+                (for [p policies]
+                  (Element. :policy
+                                (select-keys p [:name]) 
+                                (concat (for [t (filter filterfn (:titles p))]
+                                          (Element. :title {} [(apply str (:value t))]))
+                                        (for [t (filter filterfn (:texts p))]
+                                          (Element. :text {} (for [para (:value t)]
+                                                               (Element. :p {} [para]))))))))) 
 
-(defn get-children-by-lang
-  [record type lang]
-  (let [child-types (get-in protocol-version-elements [type :child-types])]
-    (filter identity (for [child-type child-types child (child-type record)]
-                       (if (= lang (:language child))
-                         [(singularize child-type) (:value child)])))))
-
-(defn get-elems-by-lang
-  [protocol-version type lang]
-  (let [records (type protocol-version)]
-    (filter identity
-            (map
-             (fn [record]
-               (let [children (get-children-by-lang record type lang)]
-                 (if (not (empty? children))
-                   {:children children
-                    :attributes (select-keys record (get-in protocol-version-elements [type :attributes]))
-                    :node-name (get-in protocol-version-elements [type :node-name])})))
-             records))))
-
-(defn generate-elem-node
-  [{:keys [node-name children attributes]}]
-  (apply xml/element node-name attributes
-         (for [[node value] children]
-           (xml/element (camel-case node) {} value))))
-
-(defn generate-type-node-by-lang
-  [protocol-version type lang]
-  (apply xml/element (camel-case type) {}
-         (for [element (get-elems-by-lang protocol-version type lang)]
-           (generate-elem-node element))))
+(defn traverse-pv
+  [pv filterfn]
+  [(emit-policies (:policies pv) filterfn)
+   (emit-metaitems (:meta-items pv) filterfn)
+   (emit-endorsements (:endorsements pv) filterfn)])
 
 (defn generate-xml
   [protocol-version]
-  (let [element-types (keys protocol-version-elements)
-        langs (:languages protocol-version)]
-    (xml/emit-str
-     (apply xml/element :ProtocolVersion {:id (:id protocol-version)}
-            (for [lang langs]
-              (apply xml/element (camel-case (keyword (:name lang))) {}
-                     (map #(generate-type-node-by-lang protocol-version % lang)
-                          element-types)))))))
+  (xml/emit-str
+    (Element. :ProtocolVersion
+              (select-keys protocol-version [:id])
+              (for [lang (:languages protocol-version)]
+                (Element. (keyword (str/lower-case (:name lang)))
+                          {} ;; no attrs
+                          (traverse-pv protocol-version
+                                       (fn [n] (= (:id lang) (:id (:language n))))))))))
 
 (def protocol-version-export-process
   {:name "get-protocol-version-export"
